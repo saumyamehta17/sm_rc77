cb70181d0685b74e46e813232d09d15ab1cd766d
sweety <smehta1289@gmail.com>
2013-06-13 21:03:41 +0530

    destroy when javascript is not enabled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc77::Application.config.secret_token = '9a6d787cc57cc38e2dad426662d5af230e64d7771ef75ca238d7129c4e88501e26f9eaebbb2b53f2b335ec0595f3ea312672a18f8f8d447cf944f0a649b99c78'
